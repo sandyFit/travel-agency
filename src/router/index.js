7ee944a64bbbcad9ddc56e5b29674f4088d07e1a
SandyFit <sandy07r@gmail.com>
2024-11-09 23:35:35 -0500

    Added magic comments to the routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,24 @@ const routes = [
     },
 
     {
-      path: '/brazil', name: 'Brazil', component: () => import('@/views/Brazil.vue')
+      path: '/brazil',
+      name: 'Brazil',
+      component: () => import(/* webpackChunkName: "brazil"*/ '@/views/Brazil.vue')
     },
     {
-      path: '/jamaica', name: 'Jamaica', component: () => import('@/views/Jamaica.vue')
+      path: '/jamaica', 
+      name: 'Jamaica', 
+      component: () => import(/* webpackChunkName: "jamaical"*/ '@/views/Jamaica.vue')
     },
     {
-      path: '/hawaii', name: 'Hawaii', component: () => import('@/views/Hawaii.vue')
+      path: '/hawaii', 
+      name: 'Hawaii', 
+      component: () => import(/* webpackChunkName: "hawaii"*/ '@/views/Hawaii.vue')
     },
     {
-      path: '/panama', name: 'Panama', component: () => import('@/views/Panama.vue')
+      path: '/panama', 
+      name: 'Panama', 
+      component: () => import(/* webpackChunkName: "panama"*/ '@/views/Panama.vue')
     },
   ]
 
